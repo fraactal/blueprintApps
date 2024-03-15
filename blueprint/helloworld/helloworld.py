f92460558d99819a0d14a3a3d9e7ae54c7679eb8
fraactal <jonathan.valdes.o@gmail.com>
2024-03-15 00:02:27 -0300

    add index

--- blueprint/helloworld/helloworld.py
+++ blueprint/helloworld/helloworld.py
@@ -5,19 +5,15 @@ helloworld_bp = Blueprint("helloworld",__name__, template_folder= "templates")
 
 @helloworld_bp.route("/")
 def index():
-    return "Hello World!"
+    return render_template("helloworld/bootcamp.html")
 
 
 @helloworld_bp.route("/hello")
 def hello():
-    return "Hello World Again!"
+    return "Hello World!"
 
 
 @helloworld_bp.route("/hello/<name>")
 def hello_name(name):
     return f"Hello {name}"
 
-
-@helloworld_bp.route("/hellohtml")
-def hello_html():
-    return render_template("helloworld/hello.html")
--- main.py
+++ main.py
@@ -9,22 +9,25 @@ app.register_blueprint(calculator_bp, url_prefix = "/calculator")
 
 
 
-#if __name__ == '__main__':
-    #app.run(debug=True)
-#    app.run(host="0.0.0.0", port=80)
 
-
-################ Handler Page ########################
 
 @app.route('/about')
 def about():
     return render_template('about.html')
     
+
+################ Handler Page ########################
+
 def PaginaNoEncontrada():
     return "<h1>La URL que intentas buscar no existe..</h1>" , 404
 
+
+################ Running App ########################
+
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    #port = int(os.environ.get('PORT', 5000))
     #app.config.from_object(config['development'])
     app.register_error_handler(404, PaginaNoEncontrada)
-    app.run(host='0.0.0.0', port=port)
+    #app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=443, ssl_context='adhoc')
+    #app.run(host='0.0.0.0', ssl_context='adhoc')
